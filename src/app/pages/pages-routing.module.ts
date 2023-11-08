35f4afd24f6209f91d0423e32186eacf3f9c7748
Shoeb Uddin Ahmed <shoeb.ua@gmail.com>
2023-11-09 00:48:36 +0600

    dashboard module

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -4,14 +4,23 @@ import { RouterModule, Routes } from '@angular/router';
 import { PagesComponent } from './pages.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    component: PagesComponent,
-  },
+    {
+        path: '',
+        component: PagesComponent,
+        children: [
+            {
+                path: '',
+                loadChildren: () =>
+                    import('./dashboard/dashboard.module').then(
+                        (m) => m.DashboardModule
+                    ),
+            },
+        ],
+    },
 ];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule],
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule],
 })
 export class PagesRoutingModule {}
