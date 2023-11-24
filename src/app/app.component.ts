39dcb45042070037f30fea01df1357978bf056d5
Shoeb Uddin Ahmed <shoeb.ua@gmail.com>
2023-11-24 09:56:04 +0600

    setup dark mode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { LayoutService } from './services/layout.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+    constructor(public layoutService: LayoutService) {}
+
+    async ngOnInit(): Promise<void> {
+        const theme = await JSON.parse(localStorage.getItem('theme') || '');
+        await this.layoutService.changeTheme(theme.theme, theme.colorScheme);
+    }
+}
--- src/app/core/header/header.module.ts
+++ src/app/core/header/header.module.ts
@@ -4,9 +4,12 @@ import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header.component';
 
+import { MenuModule } from 'primeng/menu';
+import { InputSwitchModule } from 'primeng/inputswitch';
+
 @NgModule({
     declarations: [HeaderComponent],
-    imports: [CommonModule, RouterModule],
+    imports: [CommonModule, RouterModule, MenuModule, InputSwitchModule],
     exports: [HeaderComponent],
 })
 export class HeaderModule {}
