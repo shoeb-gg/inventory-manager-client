2d4cb30f1763aa879283a27be5eb4ab8fde91242
Shoeb Uddin Ahmed <shoeb.ua@gmail.com>
2023-12-11 20:25:36 +0600

    implemented login & header interceptor

---
+++ src/app/core/auth/auth.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+
+import { Injectable } from '@angular/core';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class AuthService {
+    constructor(
+        private readonly http: HttpClient,
+        private readonly router: Router
+    ) {}
+
+    login(username: string, password: string) {
+        return this.http.post('http://localhost:6009/api/auth/login', {
+            username: username,
+            password: password,
+        });
+    }
+
+    logout() {
+        localStorage.removeItem('access_token');
+        this.router.navigateByUrl('auth/login');
+    }
+}
