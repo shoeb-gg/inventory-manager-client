168d1bad652cf1071f1ee54126a2fe44d32556b6
Shoeb Uddin Ahmed <shoeb.ua@gmail.com>
2023-12-27 06:58:08 +0600

    implement getAll business and create business

---
+++ src/app/common/models/Response_Model.ts
@@ -0,0 +1,4 @@
+export class ResponseModel {
+    message: string;
+    status: number;
+}
--- src/app/services/business.service.ts
+++ src/app/services/business.service.ts
@@ -1,13 +1,21 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+import { ResponseModel } from '../common/models/Response_Model';
+
 @Injectable({
     providedIn: 'root',
 })
 export class BusinessService {
     constructor(private readonly http: HttpClient) {}
 
+    public apiUrl = window['__env']['apiUrl'] + 'business';
+
     createBusiness(businessInfo) {
-        return this.http.post('sdcsc', businessInfo);
+        return this.http.post<ResponseModel>(this.apiUrl, businessInfo);
+    }
+
+    getUserMultipleBusiness() {
+        return this.http.get(this.apiUrl + '/all');
     }
 }
