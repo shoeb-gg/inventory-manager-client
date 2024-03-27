a561951411c6c360d35cf532f6748100e9b4124d
Shoeb Uddin Ahmed <shoeb.ua@gmail.com>
2024-03-27 23:56:07 +0600

    added date filter in all purchases

--- src/app/pages/purchases/purchases.module.ts
+++ src/app/pages/purchases/purchases.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { PurchasesRoutingModule } from './purchases-routing.module';
 import { ButtonModule } from 'primeng/button';
 import { TableModule } from 'primeng/table';
 import { ProgressSpinnerModule } from 'primeng/progressspinner';
+import { CalendarModule } from 'primeng/calendar';
 
 import { PurchasesComponent } from './purchases.component';
 import { AllPurchasesComponent } from './all-purchases/all-purchases.component';
@@ -18,6 +20,8 @@ import { SellersComponent } from './sellers/sellers.component';
         ButtonModule,
         TableModule,
         ProgressSpinnerModule,
+        CalendarModule,
+        FormsModule,
     ],
 })
 export class PurchasesModule {}
